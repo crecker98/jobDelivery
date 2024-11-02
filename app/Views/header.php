72c1e1fa5038c2be63bed47549118e0f33f0e511
RAYPROGR <90105477+RAYPROGR@users.noreply.github.com>
2024-11-02 12:00:25 +0100

    bugfix grafici
    bugfix grafici

--- app/Views/header.php
+++ app/Views/header.php
@@ -20,7 +20,7 @@
 
 <body>
 <nav class="navbar navbar-expand-lg fixed-top bg-body clean-navbar navbar-light">
-    <div class="container"><a class="navbar-brand logo" href="<?=base_url()?>">jobDelivery</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container"><a class="navbar-brand logo" href="<?=base_url()?>"><img src="assets/img/tech/Schermata 2023-12-11 alle 17.07.16.png" style="height: 50px; width: auto;"></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="<?=base_url()?>">Home</a></li>
