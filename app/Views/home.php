4a653d2982c522eab04b4c51dae97a3bc6943dbc
RAYPROGR <90105477+RAYPROGR@users.noreply.github.com>
2024-11-02 11:53:18 +0100

    graph fix home
    graph fix home

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,22 +1,23 @@
 
     <main class="page landing-page">
-        <section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/tech/image4.jpg&quot;);color:rgba(9, 162, 255, 0.85);">
+        <section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/tech/1.png&quot;);color:rgba(9, 162, 255, 0.85);">
             <div class="text">
-                <h2>TITOLO</h2>
-                <p>SOTTOTITOLO</p><a class="btn btn-outline-light btn-lg" role="button" href="<?=base_url('professionistiList')?>">I nostri professionisti</a>
+                <h2>Un modo innovativo di connettervi con i professionisti di cui avete bisogno, quando avete bisogno!</h2>
+                <a class="btn btn-outline-light btn-lg" role="button" href="professionisti.html">I nostri professionisti</a>
             </div>
         </section>
         <section class="clean-block clean-info dark">
             <div class="container">
                 <div class="block-heading">
                     <h2 class="text-info">About us</h2>
-                    <p>mission</p>
+                    <p>La nostra mission è fornire una piattaforma affidabile e intuitiva che facilita la connessione tra clienti e professionisti qualificati, come idraulici, elettricisti e altri esperti, permettendo ai clienti di risolvere le loro problematiche domestiche in modo efficiente e sicuro. Ci impegniamo a garantire un servizio di qualità, mettendo al centro la soddisfazione e la tranquillità dei nostri utenti.
+
+</p>
                 </div>
                 <div class="row align-items-center">
-                    <div class="col-md-6"><img class="img-thumbnail" src="assets/img/scenery/image5.jpg"></div>
                     <div class="col-md-6">
-                        <h3>vision</h3>
-                        <div class="getting-started-info"></div><a class="btn btn-outline-primary btn-lg" role="button" href="<?=base_url('/professionisti/signup')?>">Lavora con noi</a>
+                        <h3>Puntiamo a diventare il punto di riferimento online per la ricerca di professionisti specializzati, basando ogni interazione su fiducia e competenza e rendendo la scelta di esperti per interventi domestici facile e trasparente per tutti.</h3>
+                        <div class="getting-started-info"></div><a class="btn btn-outline-primary btn-lg" role="button" href="professionista/registration.html">Lavora con noi</a>
                     </div>
                 </div>
             </div>
@@ -29,20 +30,20 @@
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-md-5 feature-box"><i class="icon-star icon"></i>
-                        <h4>Bootstrap 5</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                        <h4>Professionista</h4>
+                        <p>Registrati come professionista, visualizza gli annunci di zona e candidati per ottenere il lavoro.</p>
                     </div>
                     <div class="col-md-5 feature-box"><i class="icon-pencil icon"></i>
-                        <h4>Customizable</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                        <h4>Cliente</h4>
+                        <p>Registrati come utente, pubblica annunci e trova il professionista più adatto alle tue esigenze.</p>
                     </div>
                     <div class="col-md-5 feature-box"><i class="icon-screen-smartphone icon"></i>
-                        <h4>Responsive</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                        <h4>Recensioni</h4>
+                        <p>Esprimi un feedback valutando da 1 a 5 stelle il lavoro eseguito dal professionista.</p>
                     </div>
                     <div class="col-md-5 feature-box"><i class="icon-refresh icon"></i>
-                        <h4>All Browser Compatibility</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+                        <h4>Abbonamento</h4>
+                        <p>Sei un professionista? Abbonati per ricevere notifiche sugli annunci che potrebbero interessarti.</p>
                     </div>
                 </div>
             </div>
