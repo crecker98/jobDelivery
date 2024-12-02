eb3b59dc505b84c970367e170c46377161924a5d
RAYPROGR <90105477+RAYPROGR@users.noreply.github.com>
2024-12-02 09:44:29 +0100

    inserite foto
    inserite foto

--- app/Views/home.php
+++ app/Views/home.php
@@ -8,15 +8,18 @@
         </section>
         <section class="clean-block clean-info dark">
             <div class="container">
-                <div class="block-heading">
-                    <h2 class="text-info">Mission</h2>
-                </div>
                 <div class="row align-items-center">
                     <div class="col-md-6">
-                        <h3>Puntiamo a diventare il punto di riferimento online per la ricerca di professionisti specializzati, basando ogni interazione su fiducia e competenza e rendendo la scelta di esperti per interventi domestici facile e trasparente per tutti.</h3>
+                    <div class="block-heading">
+                    <h2 class="text-info">Mission</h2>
+                </div>
+                        <h5>Puntiamo a diventare il punto di riferimento online per la ricerca di professionisti specializzati, basando ogni interazione su fiducia e competenza e rendendo la scelta di esperti per interventi domestici facile e trasparente per tutti.</h5>
                     </div>
                     <div class="col-md-6">
-                        <h3>La nostra mission è fornire una piattaforma affidabile e intuitiva che facilita la connessione tra clienti e professionisti qualificati, come idraulici, elettricisti e altri esperti, permettendo ai clienti di risolvere le loro problematiche domestiche in modo efficiente e sicuro. Ci impegniamo a garantire un servizio di qualità, mettendo al centro la soddisfazione e la tranquillità dei nostri utenti.</h3>
+                    <div class="block-heading">
+                    <h2 class="text-info">About us</h2>
+                </div>
+                        <h5>Offriamo una piattaforma affidabile e intuitiva per connettere clienti e professionisti qualificati, risolvendo in modo sicuro ed efficiente le problematiche domestiche, con un focus sulla qualità e la soddisfazione degli utenti.</h5>
                     
                     </div>
                     <div class="getting-started-info"></div><a class="btn btn-outline-primary btn-lg" role="button" href="<?=base_url('/professionisti/signup')?>">Lavora con noi</a>
@@ -27,22 +30,21 @@
             <div class="container">
                 <div class="block-heading">
                     <h2 class="text-info">Cosa facciamo?</h2>
-                    <p>sottotitolo</p>
                 </div>
                 <div class="row justify-content-center">
-                    <div class="col-md-5 feature-box"><i class="icon-star icon"></i>
+                    <div class="col-md-5 feature-box"><img src="assets/img/professionista.png" style="height:50px!important; width: auto"></i>
                         <h4>Professionista</h4>
                         <p>Registrati come professionista, visualizza gli annunci di zona e candidati per ottenere il lavoro.</p>
                     </div>
-                    <div class="col-md-5 feature-box"><i class="icon-pencil icon"></i>
+                    <div class="col-md-5 feature-box"><img src="assets/img/cliente.png"  style="height:50px!important; width: auto"></i>
                         <h4>Cliente</h4>
                         <p>Registrati come utente, pubblica annunci e trova il professionista più adatto alle tue esigenze.</p>
                     </div>
-                    <div class="col-md-5 feature-box"><i class="icon-screen-smartphone icon"></i>
+                    <div class="col-md-5 feature-box"><img src="assets/img/recensioni .png"  style="height:50px!important; width: auto"></i>
                         <h4>Recensioni</h4>
                         <p>Esprimi un feedback valutando da 1 a 5 stelle il lavoro eseguito dal professionista.</p>
                     </div>
-                    <div class="col-md-5 feature-box"><i class="icon-refresh icon"></i>
+                    <div class="col-md-5 feature-box"><img src="assets/img/abbonamento .png"  style="height:50px!important; width: auto"></i>
                         <h4>Abbonamento</h4>
                         <p>Sei un professionista? Abbonati per ricevere notifiche sugli annunci che potrebbero interessarti.</p>
                     </div>
